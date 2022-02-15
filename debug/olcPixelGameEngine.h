91d771a2392cc7b2eaac00341667838b143d10c0
GamerMine <maxime-sav@outlook.fr>
2022-02-15 05:53:22 +0100

    Wooops!
    I didn't want to add my copyright in olcPixelGameEngine.h Woopsie!

--- debug/olcPixelGameEngine.h
+++ debug/olcPixelGameEngine.h
@@ -1,9 +1,3 @@
-// Copyright (c) 2021-2022 Dwight Studio's Team <support@dwight-studio.fr>
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
 #pragma region license_and_help
 /*
 	olcPixelGameEngine.h
